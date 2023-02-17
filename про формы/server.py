b87829391206f925b8829e149485cae620aa93b6
ysmur <ysmur@yandex.ru>
2023-02-17 18:00:27 +0300

    Собранный пример из учебника (про формы wtf)

--- про формы/server.py
+++ про формы/server.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from loginform import LoginForm
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 @app.route('/', methods=['POST', 'GET'])
 def selection():
@@ -19,6 +20,19 @@ def selection():
         print(request.form['accept'])
         print(request.form['sex'])
         return "Форма отправлена"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/success')
+def success():
+    return 'success'
 
 
 if __name__ == '__main__':
