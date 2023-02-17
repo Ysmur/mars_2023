de39f8e97e0b4764dc322d875f4371633f2c6a3c
ysmur <ysmur@yandex.ru>
2023-02-17 17:44:16 +0300

    Заготовка для задачи Отбор астронавтов (пока не подключился стиль)

--- про формы/server.py
+++ про формы/server.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def selection():
-    param = {}
-    # param['username'] = "Ученик Яндекс.Лицея"
-    param['title'] = 'Форма'
-    return render_template('selection.html', **param)
+    if request.method == 'GET':
+        param = {}
+        # param['username'] = "Ученик Яндекс.Лицея"
+        param['title'] = 'Форма'
+        return render_template('selection.html', **param)
+    elif request.method == 'POST':
+        print(request.form['email'])
+        print(request.form['password'])
+        print(request.form['class'])
+        print(request.form['file'])
+        print(request.form['about'])
+        print(request.form['accept'])
+        print(request.form['sex'])
+        return "Форма отправлена"
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
