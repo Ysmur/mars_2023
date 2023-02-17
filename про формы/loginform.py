7750e439780bac75261c0bf8b622ebcab13424e8
ysmur <ysmur@yandex.ru>
2023-02-17 18:16:09 +0300

    задача Двойная защита

--- про формы/loginform.py
+++ про формы/loginform.py
@@ -4,7 +4,8 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Логин', validators=[DataRequired()])
+    username = StringField('id ast', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить меня')
-    submit = SubmitField('Войти')
+    username_cap = StringField('Логин', validators=[DataRequired()])
+    password_cap = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
