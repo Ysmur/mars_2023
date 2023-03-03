adc3c0672e16dcec58d5f67c5eb841bf816fa995
ysmur <ysmur@yandex.ru>
2023-03-03 16:54:08 +0300

    готовое решение задачи Журнал работ

--- BD/main.py
+++ BD/main.py
@@ -1,6 +1,6 @@
 import datetime
 
-from flask import Flask
+from flask import Flask, render_template
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
@@ -8,24 +8,17 @@ from data.jobs import Jobs
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
+@app.route('/')
+def index():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+
+    return render_template('journal_work.html', title="start", jobs=jobs)
 
 def main():
     db_name = "db/mars_explorer.db"
     db_session.global_init(db_name)
-    job = Jobs()
-    job.team_leader = 1
-    job.job = "deployment of residential modules 1 and 2"
-    job.work_size = 15
-    job.collaborators = '2, 3'
-
-    job.start_date = datetime.datetime.now().date()
-    # job.end_date = 'datetime.'
-    job.is_finished = False
-
-    db_sess = db_session.create_session()
-    db_sess.add(job)
-    db_sess.commit()
-    # app.run()
+    app.run()
 
 
 if __name__ == '__main__':
